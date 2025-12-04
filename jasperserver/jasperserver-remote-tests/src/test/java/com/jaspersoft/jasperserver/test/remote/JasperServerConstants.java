7c77a08eac38561b10ed3ebb66c34f83b925322e
Jose da Silva <jose_j_da-s@proton.me>
2025-12-04 18:08:25 +0100

    feat: refurbish remote tests

--- jasperserver/jasperserver-remote-tests/src/test/java/com/jaspersoft/jasperserver/test/remote/JasperServerConstants.java
+++ jasperserver/jasperserver-remote-tests/src/test/java/com/jaspersoft/jasperserver/test/remote/JasperServerConstants.java
@@ -1,4 +1,6 @@
 /*
+ * Copyright (C) 2025 the Jasper Server OS Authors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  * Copyright (C) 2005-2023. Cloud Software Group, Inc. All Rights Reserved.
  * http://www.jaspersoft.com.
  *
@@ -19,7 +21,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.jaspersoft.jasperserver.war;
+package com.jaspersoft.jasperserver.test.remote;
 
 import java.util.Properties;
 
@@ -48,9 +50,16 @@ public class JasperServerConstants {
     protected void setupProperties() {
         Properties sys = System.getProperties();
 
-        HOST = sys.getProperty(PROP_HOST_NAME, "localhost");
-        PORT = sys.getProperty(PROP_HOST_PORT, "8080");
-        APP_CONTEXT_PATH = sys.getProperty(PROP_APP_CONTEXT_PATH, "jasperserver");
+        HOST = sys.getProperty(PROP_HOST_NAME);
+        PORT = sys.getProperty(PROP_HOST_PORT);
+        APP_CONTEXT_PATH = sys.getProperty(PROP_APP_CONTEXT_PATH);
+        DB_TYPE     = sys.getProperty(PROP_DB_TYPE);
+        DB_HOST     = sys.getProperty(PROP_DB_HOST);
+        DB_PORT     = sys.getProperty(PROP_DB_PORT);
+        DB_NAME     = sys.getProperty(PROP_DB_NAME);
+        DB_USERNAME = sys.getProperty(PROP_DB_USERNAME);
+        DB_PASSWORD = sys.getProperty(PROP_DB_PASSWORD);
+
     }
 
     protected void setDefaults() {
@@ -75,18 +84,40 @@ public class JasperServerConstants {
         WS_PERMISSIONS_MANAGEMENT_END_POINT_URL =
             WS_PROTOCOL + USERNAME + ":" + PASSWORD + "@" + WS_BASE_URL + "PermissionsManagementService";
 
+        REST_BASE_URL = BASE_URL + "/" + APP_CONTEXT_PATH + "/rest_v2";
+        REST_RESOURCES_URL = REST_BASE_URL + "/resources";
+        REST_REPORTS_URL = REST_BASE_URL + "/reports";
+        REST_JOBS_URL = REST_BASE_URL + "/jobs";
+        REST_EXPORT_URL = REST_BASE_URL + "/export";
+        REST_IMPORT_URL = REST_BASE_URL + "/import";
+
         USERNAME2     = "joeuser";
         PASSWORD2  = "joeuser";
         BAD_PASSWORD2  = "wrongPassword";
+        IMPORT_EXPORT_KEY_ALIAS = "deprecatedImportExportEncSecret";
     }
 
-    public String PROP_HOST_NAME = "remote.test.host";
-    public String PROP_HOST_PORT = "remote.test.port";
+    public String PROP_HOST_NAME        = "remote.test.host";
+    public String PROP_HOST_PORT        = "remote.test.port";
     public String PROP_APP_CONTEXT_PATH = "remote.test.app-context-path";
+    public String PROP_DB_TYPE          = "remote.test.dbType";
+    public String PROP_DB_HOST          = "remote.test.dbHost";
+    public String PROP_DB_PORT          = "remote.test.dbPort";
+
+    public String PROP_DB_NAME          = "remote.test.dbName";
+    public String PROP_DB_USERNAME      = "remote.test.dbUsername";
+    public String PROP_DB_PASSWORD      = "remote.test.dbPassword";
 
     public String HOST;
     public String PORT;
     public String APP_CONTEXT_PATH;
+    public String DB_TYPE;
+    public String DB_HOST;
+    public String DB_PORT;
+
+    public String DB_NAME;
+    public String DB_USERNAME;
+    public String DB_PASSWORD;
     public String USERNAME;
     public String PASSWORD;
     public String USER_USERNAME;
@@ -104,7 +135,15 @@ public class JasperServerConstants {
 
     public String WS_PERMISSIONS_MANAGEMENT_END_POINT_URL;
 
+    public String REST_BASE_URL;
+    public String REST_RESOURCES_URL;
+    public String REST_REPORTS_URL;
+    public String REST_JOBS_URL;
+    public String REST_EXPORT_URL;
+    public String REST_IMPORT_URL;
+
     public String USERNAME2;
     public String PASSWORD2;
     public String BAD_PASSWORD2;
+    public String IMPORT_EXPORT_KEY_ALIAS;
 }
